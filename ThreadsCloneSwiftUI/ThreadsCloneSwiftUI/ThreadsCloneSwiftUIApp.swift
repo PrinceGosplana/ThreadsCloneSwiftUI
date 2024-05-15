aafda29c029b8dcb5e752bbc9795a35a3c026685
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-05-15 13:47:44 +0300

    init project

---
+++ ThreadsCloneSwiftUI/ThreadsCloneSwiftUI/ThreadsCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneSwiftUIApp.swift
+//  ThreadsCloneSwiftUI
+//
+//  Created by Oleksandr Isaiev on 15.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
