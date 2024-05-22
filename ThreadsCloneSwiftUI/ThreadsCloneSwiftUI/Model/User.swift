0963e429368fcf3939fa9c4f26148403a9ef8d9e
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-05-22 11:31:06 +0300

    add users icons

--- ThreadsCloneSwiftUI/ThreadsCloneSwiftUI/Model/User.swift
+++ ThreadsCloneSwiftUI/ThreadsCloneSwiftUI/Model/User.swift
@@ -19,9 +19,9 @@ struct User: Identifiable, Hashable {
 extension User {
     static let mockUsers: [User] = [
         .init(id: "BC0B7A87-B914-47AA-A4A5-E37294CF236E", fullName: "Graham Nou", userName: "Kitten", email: "kitten@test.com", profileImageUrl: "lewis-hamilton", bio: "Am I a cat?"),
-        .init(id: "BC0B7A87-B914-47AA-A4A5-E37294CF25Y5", fullName: "Mark Zakes", userName: "Gondor", email: "gondor@test.com", bio: "A knight"),
-        .init(id: "BC0B7A87-B914-47AA-A4A5-E37294CF2Q1W", fullName: "Marlando Poor", userName: "Rohan", email: "rohan@test.com", bio: "La way kiki"),
-        .init(id: "BC0B7A87-B914-47AA-A4A5-E37294CF2P44", fullName: "Adelaida Markez", userName: "Pensilvania", email: "pensilvania@test.com", bio: "Beautiful life"),
-        .init(id: "BC0B7A87-B914-47AA-A4A5-E37294CF2FF9", fullName: "Amber Rock", userName: "Rock-and-roll", email: "rock@test.com", bio: "Happy nation"),
+        .init(id: "BC0B7A87-B914-47AA-A4A5-E37294CF25Y5", fullName: "Mark Zakes", userName: "Gondor", email: "gondor@test.com", profileImageUrl: "batman", bio: "A knight"),
+        .init(id: "BC0B7A87-B914-47AA-A4A5-E37294CF2Q1W", fullName: "Marlando Poor", userName: "Rohan", email: "rohan@test.com", profileImageUrl: "black-panther", bio: "La way kiki"),
+        .init(id: "BC0B7A87-B914-47AA-A4A5-E37294CF2P44", fullName: "Adelaida Markez", userName: "Pensilvania", email: "pensilvania@test.com", profileImageUrl: "megan-fox", bio: "Beautiful life"),
+        .init(id: "BC0B7A87-B914-47AA-A4A5-E37294CF2FF9", fullName: "Amber Rock", userName: "Rock-and-roll", email: "rock@test.com", profileImageUrl: "kelly", bio: "Happy nation"),
     ]
 }
